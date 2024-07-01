64473bc3bd3d6862e821d399fc934d7414ffb946
lycanthrope8 <jubayerhossain9999@gmail.com>
2024-07-02 02:09:32 +0600

    added two pages

---
+++ client/src/pages/AdminPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminPanel = () => {
+  return (
+    <div>
+      <h1>This is Admin Panel</h1>
+    </div>
+  )
+}
+
+export default AdminPanel
---
+++ client/src/pages/UserPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPanel = () => {
+  return (
+    <div>
+      <h1>This is User Panel</h1>
+    </div>
+  )
+}
+
+export default UserPanel
