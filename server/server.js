3667dd59983b1cdeafb2969f59a4049d381709cf
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2024-08-30 10:21:50 +0600

    feat: Update dependencies for MongoDB and Mongoose
    This commit updates the dependencies for MongoDB and Mongoose to their latest versions. This ensures compatibility with the latest features and bug fixes provided by these libraries.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const passport = require('./config/passport-config'); 
+const passport = require('./config/passport-config');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const https = require('https');
@@ -12,6 +12,7 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
+const mongoose = require('mongoose');
 
 const corsOptions = {
   origin: 'https://localhost:3001',
@@ -27,9 +28,9 @@ app.options('*', cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -39,11 +40,17 @@ app.use('/', authRoutes);
 app.use('/', userRoutes);
 app.use('/xacml', xacmlRoutes); // Use XACML routes
 
+// MONGOOSE CONNECTION
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  https.createServer(httpsOptions, app).listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error connecting to MongoDB:', error.message);
+});
+
 const httpsOptions = {
   key: fs.readFileSync(path.join(__dirname, 'security', 'server.key')),
   cert: fs.readFileSync(path.join(__dirname, 'security', 'server.cert'))
 };
-
-https.createServer(httpsOptions, app).listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
