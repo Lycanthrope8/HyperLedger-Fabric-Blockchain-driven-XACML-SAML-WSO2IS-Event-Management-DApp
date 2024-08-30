da2149215bc87aa775987e43b6c9c9e7207cdcdf
lycanthrope8 <jubayerhossain9999@gmail.com>
2024-08-30 11:33:29 +0600

    authorization in admin page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import useAuth from "./hooks/useAuth";
@@ -12,28 +7,40 @@ import NotAuthorized from "./pages/NotAuthorized";
 import AdminPage from "./pages/AdminPage";
 import CreateEvent from "./pages/CreateEvent";
 import EventDetails from "./pages/EventDetails";
+import { useUser } from "./contexts/UserContext";
+import useAuthorization from "./hooks/useAuthorization";
+
 
 function App() {
-  const { authenticated, loading } = useAuth();
+  const { authenticated, loading: authLoading } = useAuth();
+  const { userProfile } = useUser();
+  const username = userProfile.username;
+  const { isAuthorized, loading: authzLoading, error } = useAuthorization(username, 'write', 'adminPanel');
 
-  if (loading) {
+  // Combine loading states and check if there's an error (optional)
+  if (authLoading || authzLoading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.error("Authorization Error:", error);
+    return <div>Error checking authorization.</div>;
   }
 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/home"
-          element={authenticated ? <Home /> : <Navigate to="/" />}
+        <Route path="/" element={authenticated ? <Navigate to="/home" /> : <Login />} />
+        <Route path="/home" element={authenticated ? <Home /> : <Navigate to="/" />} />
+        <Route path="/admin"
+          element={authenticated ? (isAuthorized ? <AdminPage /> : <Navigate to="/not-authorized" />) : <Navigate to="/" />}
         />
-        <Route path="/" element={<Login authenticated={authenticated} />} />
-        <Route
-          path="/admin"
-          element={authenticated ? <AdminPage /> : <Navigate to="/" />}
+        <Route path="/create-event"
+          element={authenticated ? (isAuthorized ? <CreateEvent /> : <Navigate to="/not-authorized" />) : <Navigate to="/" />}
         />
-        <Route path="/create-event" element={authenticated ? <CreateEvent /> : <Navigate to="/" />} />
-        <Route path="/event-details/:eventId" element={authenticated ? <EventDetails /> : <Navigate to="/" />} />
+        <Route path="/event-details/:eventId"
+          element={authenticated ? <EventDetails /> : <Navigate to="/" />}
+        />
         <Route path="/not-authorized" element={<NotAuthorized />} />
       </Routes>
     </Router>
