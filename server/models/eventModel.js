b331e9366c5c463ba33e2ce176ef366a27a9a726
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2024-08-31 07:17:36 +0600

    mongodb initialization and event model

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -7,7 +7,7 @@ const eventSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        required: true //comment
+        required: true
     },
     image: {
         type: String,
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,29 @@
 
 const express = require('express');
+const Event = require('../models/eventModel');
 const router = express.Router();
 
 router.get('/user-profile', (req, res) => {
-          res.json(req.user);
+    res.json(req.user);
 });
 
+router.post('/create-event', async (req, res) => {
+    const { organizer, image, title, description, date, location } = req.body;
+
+    try {
+        const event = await Event.create({
+            organizer,
+            image,
+            title,
+            description,
+            date,
+            location
+        });
+    } catch (error) {
+        res.status(500).send('Error creating event: ' + error.message);
+    }
+
+    res.send('Event created successfully!');
+});
 
 module.exports = router;
