870a3339579df9fbfdc384988bcd65e4768d1f6e
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2024-10-12 02:59:21 +0600

    Refactor PostForm styles and improve error handling for event creation

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -67,10 +67,9 @@ function PostForm({ onAddPost }) {
         setDate("");
         setEventName("");
         setLocation("");
-        setOrganizer("");
         setFiles([]);
       } catch (error) {
-        console.error("Error creating event:", error);
+        console.error("Error creating event:", error.response ? error.response.data : error.message);
       }
     }
   };
@@ -78,73 +77,73 @@ function PostForm({ onAddPost }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col bg-[#2a2438] p-4 rounded-lg shadow-md"
+      className="flex flex-col bg-[#eeeeee] p-4 rounded-lg shadow-md"
       encType="multipart/form-data"
     >
-      <h2 className="text-4xl text-center text-zinc-50 mb-8">Create a Post</h2>
+      <h2 className="text-4xl text-center text-zinc-700 mb-8">Create a Post</h2>
 
       <div className="grid grid-cols-2 gap-24">
-        <div {...getRootProps()} className="border-dashed border-2 border-zinc-600 bg-[#3b3a3a] p-4 rounded-lg mb-4 cursor-pointer">
+        <div {...getRootProps()} className="border-dashed border-2 border-zinc-600 bg-zinc-300 p-4 rounded-lg mb-4 cursor-pointer">
           <input name='image' {...getInputProps()} />
           {files.length > 0 ? (
             <div className="flex justify-center items-center">
               <img src={files[0].preview} alt="preview" className="w-full h-96" />
             </div>
           ) : (
-            <p className="text-zinc-50 text-center">
-              {isDragActive ? "Drop the image here ..." : "Drag 'n' drop an image here, or click to select an image"}
+            <p className="text-zinc-700 h-full flex justify-center items-center">
+              {isDragActive ? "Drop the image here ..." : "Drag n drop an image here or click to select an image"}
             </p>
           )}
         </div>
 
         <div>
           {/* <div className="mb-4">
-            <label className="block text-zinc-50 mb-1">Organizer:</label>
+            <label className="block text-zinc-700 mb-1">Organizer:</label>
             <input
               type="text"
               value={organizer}
               onChange={(e) => setOrganizer(e.target.value)}
-              className="w-full p-2 rounded border border-zinc-600 bg-[#3b3a3a] text-zinc-50"
+              className="w-full p-2 rounded border border-zinc-600 bg-zinc-300 text-zinc-700"
               required
             />
           </div> */}
           <div className="mb-4">
-            <label className="block text-zinc-50 mb-1">Event Name:</label>
+            <label className="block text-zinc-700 mb-1">Event Name:</label>
             <input
               type="text"
               value={eventName}
               onChange={(e) => setEventName(e.target.value)}
-              className="w-full p-2 rounded border border-zinc-600 bg-[#3b3a3a] text-zinc-50"
+              className="w-full p-2 rounded border border-zinc-600 bg-zinc-300 text-zinc-700"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-zinc-50 mb-1">Date:</label>
+            <label className="block text-zinc-700 mb-1">Date:</label>
             <input
               type="date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
-              className="w-full p-2 rounded border border-zinc-600 bg-[#3b3a3a] text-zinc-50"
+              className="w-full p-2 rounded border border-zinc-600 bg-zinc-300 text-zinc-700"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-zinc-50 mb-1">Location:</label>
+            <label className="block text-zinc-700 mb-1">Location:</label>
             <input
               type="text"
               value={location}
               onChange={(e) => setLocation(e.target.value)}
-              className="w-full p-2 rounded border border-zinc-600 bg-[#3b3a3a] text-zinc-50"
+              className="w-full p-2 rounded border border-zinc-600 bg-zinc-300 text-zinc-700"
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-zinc-50 mb-1">Post Content:</label>
+            <label className="block text-zinc-700 mb-1">Post Content:</label>
             <textarea
               value={text}
               onChange={(e) => setText(e.target.value)}
               rows="4"
-              className="w-full p-2 rounded border border-zinc-600 bg-[#3b3a3a] text-zinc-50"
+              className="w-full p-2 rounded border border-zinc-600 bg-zinc-300 text-zinc-700"
               required
             />
           </div>
@@ -152,7 +151,7 @@ function PostForm({ onAddPost }) {
       </div>
       <button
         type="submit"
-        className="w-full text-2xl bg-[#5c5470] p-4 rounded-lg hover:brightness-105 text-zinc-50"
+        className="w-full text-2xl bg-zinc-300 p-4 rounded-lg hover:brightness-90 text-zinc-700 transition-all"
       >
         Post
       </button>
